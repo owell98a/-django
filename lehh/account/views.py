2505baa242580fb650cce84c263db8dce412e70b
company <1700043572@qq.com>
2019-10-14 16:30:36 +0800

    完成注册功能

--- lehh/account/views.py
+++ lehh/account/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
-
+from .forms import RegistrationForm
+from .forms import UserProfileForm
 # Create your views here.
 
 def user_login(request):
@@ -22,3 +23,23 @@ def user_login(request):
     if request.method == "GET":
         login_form = LoginForm()
         return render(request, "account/login.html", {"form": login_form})
+
+def user_register(request):
+    if request.method == "POST":
+        user_form = RegistrationForm(request.POST)
+        userprofile_form = UserProfileForm(request.POST)
+        if user_form.is_valid()* userprofile_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            new_profile = userprofile_form.save(commit=False)
+            new_profile.user = new_user
+            new_profile.save()
+            return HttpResponse("注册成功！")
+        else:
+            return HttpResponse("抱歉，你不能注册")
+    else:
+        user_form = RegistrationForm()
+        userprofile_form = UserProfileForm()
+        #return render(request, "account/register.html", {"form": user_form})
+        return render(request, "account/register.html", {"form": user_form, "profile": userprofile_form})
